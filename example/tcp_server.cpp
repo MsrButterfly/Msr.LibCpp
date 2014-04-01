1299e22dea1b3d255890cc9a528409c65f7e599a
Darren Liu <butterfly@msrlab.org>
2014-04-02 04:28:49 +0800

    [Network] Updated example.

--- example/tcp_server.cpp
+++ example/tcp_server.cpp
@@ -15,24 +15,26 @@ public:
     void did_accept(weak_ptr<server> s,
                     shared_ptr<connection> c,
                     error e) override {
-        printf("Accepted: %s\n", c->remote_endpoint().address().to_string().c_str());
+        printf("[Accepted] %s\n", c->remote_endpoint().address().to_string().c_str());
         if (!s.expired()) {
             try {
-                s.lock()->accept();
+                 s.lock()->accept();
+                s.lock()->send(c, "Welcome to mashiroLab!!!");
+                s.lock()->receive(c, 100);
             } catch (...) {}
         }
     }
-    void did_send(weak_ptr<server> s,
-                  shared_ptr<connection> c,
-                  error e,
-                  size_t size) override {
-        
+    void did_send(std::weak_ptr<server> s, std::shared_ptr<connection> c, error e, data d) {
+        if (d.size() == 25) {
+            printf("[Sent] %s: %s\n", c->remote_endpoint().address().to_string().c_str(), d.get<char, 25>().data());
+        } else {
+            printf("[Sent] %s: %s\n", c->remote_endpoint().address().to_string().c_str(), d.get<char, 100>().data());
+        }
     }
-    void did_receive(weak_ptr<server> s,
-                     shared_ptr<connection> c,
-                     error e,
-                     size_t size) override {
-        
+    void did_receive(std::weak_ptr<server> s, std::shared_ptr<connection> c, error e, data d) {
+        printf("[Received] %s: %s\n", c->remote_endpoint().address().to_string().c_str(), d.get<char, 100>().data());
+        s.lock()->send(c, d);
+        s.lock()->receive(c, 100);
     }
 public:
     ~controller() {}
