209e8a00bcd1e6058c06b7ed653dee337494b2a2
Darren Liu <butterfly@msrlab.org>
2014-04-03 00:45:06 +0800

    [Network] Updated example.

--- example/tcp_server.cpp
+++ example/tcp_server.cpp
@@ -5,6 +5,11 @@ using namespace std;
 using namespace msr::network;
 using protocol::ip::tcp;
 
+struct information {
+    char name[10];
+    uint16_t age;
+};
+
 class controller: public observer<server<tcp>> {
 public:
     using self = controller;
@@ -13,38 +18,49 @@ public:
     controller(condition_variable &server_has_been_shutdown):
         server_has_been_shutdown_(server_has_been_shutdown) {}
 public:
-    void did_accept(weak_ptr<server> s,
-                    shared_ptr<connection> c,
-                    error e) override {
+    void server_did_accept(server_ptr s, connection_ptr c, error e) override {
+        if (e) {
+            s.lock()->disconnect(c);
+            return;
+        }
         printf("[Accepted] %s\n", c->remote_endpoint().address().to_string().c_str());
         if (!s.expired()) {
             s.lock()->accept();
             s.lock()->send(c, "Welcome to mashiroLab!!!");
-            s.lock()->receive(c, 100);
+            s.lock()->receive(c, sizeof(information) / sizeof(data::unit_t));
         }
     }
-    void did_send(std::weak_ptr<server> s, std::shared_ptr<connection> c, error e, data d) {
-        if (d.size() == 25) {
-            printf("[Sent] %s: %s\n", c->remote_endpoint().address().to_string().c_str(), d.get<char, 25>().data());
-        } else {
-            printf("[Sent] %s: %s\n", c->remote_endpoint().address().to_string().c_str(), d.get<char, 100>().data());
-        }
-    }
-    void did_receive(std::weak_ptr<server> s, std::shared_ptr<connection> c, error e, data d) {
-        string message = d.get<char, 100>().data();
-        printf("[Received] %s: %s\n", c->remote_endpoint().address().to_string().c_str(), message.c_str());
-        if (message == "shutdown") {
-            s.lock()->shutdown();
+    void server_did_send(server_ptr s, connection_ptr c, error e, data d) override {
+        if (e) {
+            s.lock()->disconnect(c);
             return;
         }
-        s.lock()->send(c, d);
-        s.lock()->receive(c, 100);
+        if (d.size() == sizeof("Welcome to mashiroLab!!!")) {
+            printf("[Sent] %s: %s\n", c->remote_endpoint().address().to_string().c_str(), d.get<char, 25>().data());
+        } else {
+            auto i = d.get<information>();
+            printf("[Sent] %s: Name: %s, Age: %d\n", c->remote_endpoint().address().to_string().c_str(), i.name, i.age);
+        }
     }
-    virtual void did_run(std::weak_ptr<server> s, error e) {
+    void server_did_receive(server_ptr s, connection_ptr c, error e, data d) override {
+        if (e) {
+            s.lock()->disconnect(c);
+            return;
+        }
+        auto i = d.get<information>();
+        printf("[Received] %s: Name: %s, Age: %d\n", c->remote_endpoint().address().to_string().c_str(), i.name, i.age);
+        s.lock()->send(c, i);
+        s.lock()->receive(c, sizeof(information) / sizeof(data::unit_t));
+    }
+    void server_did_disconnect(server_ptr s, endpoint p, error e) override {
+        printf("[Disconnected] %s\n", p.address().to_string().c_str());
+    }
+    void server_did_cancel(server_ptr s, connection_ptr c, error e) override {}
+    void server_did_run(server_ptr s, error e) override {
         printf("[Running] %s\n", e.message().c_str());
         s.lock()->accept();
     }
-    virtual void did_shutdown(std::weak_ptr<server> s, error e) {
+    void server_did_shutdown(server_ptr s, error e) override {
         printf("[Shutdown] %s\n", e.message().c_str());
         server_has_been_shutdown_.notify_all();
     }
