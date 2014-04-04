daee2d2ec238509cdf98d6d80250880caff67c61
Darren Liu <butterfly@msrlab.org>
2014-04-04 20:39:23 +0800

    [Network] Updated example.

---
+++ example/tcp_client.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <msr/network.hpp>
+
+struct information {
+    char name[10] = "Darren";
+    uint16_t age = 19;
+};
+
+using namespace std;
+using namespace msr::network;
+using protocol::ip::tcp;
+
+class controller: public observer<client<tcp>> {
+public:
+    using self = controller;
+    using base = observer;
+public:
+    controller(condition_variable &client_has_been_shutdown):
+        client_has_been_shutdown_(client_has_been_shutdown) {}
+public:
+    void client_did_connect(client_weak_ptr c, connection_ptr n, error e) override {
+        client_shared_ptr client_;
+        try {
+            client_ = c.lock();
+        } catch (...) {
+            return;
+        }
+        if (e) {
+            client_->disconnect(n);
+            return;
+        }
+        printf("[Connected] %s\n", n->remote_endpoint().address().to_string().c_str());
+        client_->receive(n, 25);
+    }
+    void client_did_send(client_weak_ptr c, connection_ptr n, error e, data d) override {
+        client_shared_ptr client_;
+        try {
+            client_ = c.lock();
+        } catch (...) {
+            return;
+        }
+        if (e) {
+            client_->disconnect(n);
+            return;
+        }
+        auto i = d.get<information>();
+        printf("[Sent] %s: Name: %s, Age: %d\n", n->remote_endpoint().address().to_string().c_str(), i.name, i.age);
+        client_->shutdown();
+    }
+    void client_did_receive(client_weak_ptr c, connection_ptr n, error e, data d) override {
+        client_shared_ptr client_;
+        try {
+            client_ = c.lock();
+        } catch (...) {
+            return;
+        }
+        if (e) {
+            client_->disconnect(n);
+            return;
+        }
+        client_->send(n, information());
+    }
+    void client_did_disconnect(client_weak_ptr c, endpoint p, error e) override {
+        printf("[Disconnected] %s\n", p.address().to_string().c_str());
+    }
+    void client_did_cancel(client_weak_ptr c, connection_ptr n, error e) override {
+        client_shared_ptr client_;
+        try {
+            client_ = c.lock();
+        } catch (...) {
+            return;
+        }
+        if (e) {
+            client_->disconnect(n);
+            return;
+        }
+    }
+    void client_did_run(client_weak_ptr c) override {
+        client_shared_ptr client_;
+        try {
+            client_ = c.lock();
+        } catch (...) {
+            return;
+        }
+        printf("[Running]\n");
+        client_->connect("localhost", 9999);
+    }
+    void client_did_shutdown(client_weak_ptr c) override {
+        client_has_been_shutdown_.notify_all();
+    }
+public:
+    ~controller() {}
+private:
+    condition_variable &client_has_been_shutdown_;
+};
+
+int main(int argc, const char *argv[]) {
+    mutex m;
+    unique_lock<mutex> lock(m);
+    condition_variable client_has_been_shutdown;
+    auto s = make_shared<client<tcp>>();
+    s->create_observer<controller>(client_has_been_shutdown);
+    s->run();
+    client_has_been_shutdown.wait(lock);
+}
--- example/tcp_server.cpp
+++ example/tcp_server.cpp
@@ -18,49 +18,78 @@ public:
     controller(condition_variable &server_has_been_shutdown):
         server_has_been_shutdown_(server_has_been_shutdown) {}
 public:
-    void server_did_accept(server_ptr s, connection_ptr c, error e) override {
+    void server_did_accept(server_weak_ptr s, connection_ptr c, error e) override {
+        server_shared_ptr server_;
+        try {
+            server_ = s.lock();
+        } catch (...) {
+            return;
+        }
         if (e) {
-            s.lock()->disconnect(c);
+            server_->disconnect(c);
             return;
         }
         printf("[Accepted] %s\n", c->remote_endpoint().address().to_string().c_str());
-        if (!s.expired()) {
-            s.lock()->accept();
-            s.lock()->send(c, "Welcome to mashiroLab!!!");
-            s.lock()->receive(c, sizeof(information) / sizeof(data::unit_t));
-        }
+        server_->accept();
+        server_->send(c, "Welcome to mashiroLab!!!");
+        server_->receive(c, sizeof(information));
     }
-    void server_did_send(server_ptr s, connection_ptr c, error e, data d) override {
+    void server_did_send(server_weak_ptr s, connection_ptr c, error e, data d) override {
+        server_shared_ptr server_;
+        try {
+            server_ = s.lock();
+        } catch (...) {
+            return;
+        }
         if (e) {
-            s.lock()->disconnect(c);
+            server_->disconnect(c);
             return;
         }
         if (d.size() == sizeof("Welcome to mashiroLab!!!")) {
             printf("[Sent] %s: %s\n", c->remote_endpoint().address().to_string().c_str(), d.get<char, 25>().data());
-        } else {
-            auto i = d.get<information>();
-            printf("[Sent] %s: Name: %s, Age: %d\n", c->remote_endpoint().address().to_string().c_str(), i.name, i.age);
         }
     }
-    void server_did_receive(server_ptr s, connection_ptr c, error e, data d) override {
+    void server_did_receive(server_weak_ptr s, connection_ptr c, error e, data d) override {
+        server_shared_ptr server_;
+        try {
+            server_ = s.lock();
+        } catch (...) {
+            return;
+        }
         if (e) {
-            s.lock()->disconnect(c);
+            server_->disconnect(c);
             return;
         }
         auto i = d.get<information>();
         printf("[Received] %s: Name: %s, Age: %d\n", c->remote_endpoint().address().to_string().c_str(), i.name, i.age);
-        s.lock()->send(c, i);
-        s.lock()->receive(c, sizeof(information) / sizeof(data::unit_t));
-    }
-    void server_did_disconnect(server_ptr s, endpoint p, error e) override {
+        server_->receive(c, sizeof(information));
+    } 
+    void server_did_disconnect(server_weak_ptr s, endpoint p, error e) override {
         printf("[Disconnected] %s\n", p.address().to_string().c_str());
     }
-    void server_did_cancel(server_ptr s, connection_ptr c, error e) override {}
-    void server_did_run(server_ptr s, error e) override {
-        printf("[Running] %s\n", e.message().c_str());
-        s.lock()->accept();
+    void server_did_cancel(server_weak_ptr s, connection_ptr c, error e) override {
+        server_shared_ptr server_;
+        try {
+            server_ = s.lock();
+        } catch (...) {
+            return;
+        }
+        if (e) {
+            server_->disconnect(c);
+            return;
+        }
     }
-    void server_did_shutdown(server_ptr s, error e) override {
+    void server_did_run(server_weak_ptr s, error e) override {
+        server_shared_ptr server_;
+        try {
+            server_ = s.lock();
+        } catch (...) {
+            return;
+        }
+        printf("[Running] %s\n", e.message().c_str());
+        server_->accept();
+    }
+    void server_did_shutdown(server_weak_ptr s, error e) override {
         printf("[Shutdown] %s\n", e.message().c_str());
         server_has_been_shutdown_.notify_all();
     }
