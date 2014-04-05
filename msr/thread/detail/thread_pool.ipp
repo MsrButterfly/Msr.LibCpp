535635e8bf9d868891642e3bc6d5e115a1caef23
Darren Liu <butterfly@msrlab.org>
2014-04-06 00:55:44 +0800

    [Thread] The declaration & implementation of thread pool class have been seperated.

---
+++ msr/thread/detail/thread_pool.ipp
@@ -0,0 +1,163 @@
+#ifndef MSR_THREAD_DETAIL_THREAD_POOL_IPP_INCLUDED
+#define MSR_THREAD_DETAIL_THREAD_POOL_IPP_INCLUDED
+
+#include <msr/thread/detail/debug.hpp>
+
+namespace msr {
+    thread_pool::thread_pool(const std::size_t size):
+        loop_(true), stopped_(true), size_(size) {
+        run();
+    }
+    void thread_pool::run() {
+        readwrite_lock lock(thread_mutex_);
+        if (!stopped()) {
+            return;
+        }
+        loop_ = true;
+        for (unsigned int i = 0; i < size_; i++) {
+            threads_.insert(std::unique_ptr<thread>(new thread(&self::task_loop, this)));
+        }
+        stopped_ = false;
+    }
+    void thread_pool::stop() {
+        readwrite_lock thread_lock(thread_mutex_);
+        if (unsafe_this_thread_is_in()) {
+            throw exception(1, "stop(): Thread is trying to join itself.");
+        }
+        if (stopped()) {
+            return;
+        }
+        {
+            readwrite_lock task_lock(task_mutex_);
+            loop_ = false;
+            unsafe_clear_task_queue();
+            new_task_posted_.notify_all();
+        }
+        for (auto &t : threads_) {
+            try {
+                t->join();
+            } catch (...) {
+                t->detach();
+            }
+        }
+        threads_.clear();
+        stopped_ = true;
+    }
+    bool thread_pool::stopped() const {
+        return stopped_;
+    }
+    bool thread_pool::this_thread_is_in() const {
+        readonly_lock lock(thread_mutex_);
+        return unsafe_this_thread_is_in();
+    }
+    void thread_pool::post(task task_) {
+        readwrite_lock lock(task_mutex_);
+        task_queue_.push_back(task_);
+        new_task_posted_.notify_one();
+    }
+    void thread_pool::wait() const {
+        readonly_lock lock(task_mutex_);
+        if (unsafe_this_thread_is_in()) {
+            throw exception(2, "wait(): Thread is trying to wait itself.");
+        }
+        if (!unsafe_task_queue_is_empty()) {
+            task_queue_is_empty_.wait(lock);
+        }
+    }
+    void thread_pool::clear_task_queue() {
+        readwrite_lock lock(task_mutex_);
+        unsafe_clear_task_queue();
+        task_queue_is_empty_.notify_all();
+    }
+    bool thread_pool::task_queue_is_empty() const {
+        readonly_lock lock(task_mutex_);
+        return unsafe_task_queue_is_empty();
+    }
+    std::size_t thread_pool::size() const {
+        return size_;
+    }
+    void thread_pool::resize(const std::size_t size) {
+        if (!stopped()) {
+            throw exception(3, "resize(): Trying to resize a running thread pool.");
+        }
+        size_ = size;
+    }
+    void thread_pool::task_loop() {
+        using namespace boost::chrono;
+        using namespace this_thread;
+        while (loop_) {
+            task _task;
+            {
+                readwrite_lock lock(task_mutex_);
+                /**
+                 * 'while' and '_loop' are required!
+                 * try to think about this scene.
+                 * thread_a is waiting for a new task.
+                 * then a new task comming, but thread_b
+                 * is holding the lock, and thread_b
+                 * finds that the task queue is not empty,
+                 * so thread_b tries to get the task.
+                 * meanwhile, thread_a is not blocking
+                 * anymore, so thread_a is will also try
+                 * to get the task. when one gets the task,
+                 * another's action will cause a iterator
+                 * reference error. so we use 'while' to
+                 * prevent this situation.
+                 */
+                while (unsafe_task_queue_is_empty() && loop_) {
+#if MSR_THREAD_DEBUG(THREAD_POOL)
+                    std::stringstream ss;
+                    ss << "msr::thread::thread_pool: " << "[" << get_id() << ":wait]";
+                    printf("%s\n", ss.str().c_str());
+#endif
+                    new_task_posted_.wait(lock);
+                }
+                if (!loop_) {
+                    break;
+                }
+                _task = task_queue_.front();
+                task_queue_.pop_front();
+                if (unsafe_task_queue_is_empty()) {
+                    task_queue_is_empty_.notify_all();
+                }
+            }
+#if MSR_THREAD_DEBUG(THREAD_POOL)
+            auto begin = high_resolution_clock::now();
+#endif
+            _task();
+#if MSR_THREAD_DEBUG(THREAD_POOL)
+            auto end = high_resolution_clock::now();
+            auto duration = end - begin;
+            std::stringstream ss;
+            ss << "msr::thread::thread_pool: " << "[" << get_id() << ":done] " << duration_cast<milliseconds>(duration).count() << "ms";
+            printf("%s\n", ss.str().c_str());
+#endif
+        }
+#if MSR_THREAD_DEBUG(THREAD_POOL)
+        std::stringstream ss;
+        ss << "msr::thread::thread_pool: " << "[" << get_id() << ":exit]";
+        printf("%s\n", ss.str().c_str());
+#endif
+    }
+    bool thread_pool::unsafe_this_thread_is_in() const {
+        auto id = this_thread::get_id();
+        for (const auto &t : threads_) {
+            if (t->get_id() == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+    void thread_pool::unsafe_clear_task_queue() {
+        task_queue_.clear();
+    }
+    bool thread_pool::unsafe_task_queue_is_empty() const {
+        return task_queue_.empty();
+    }
+    thread_pool::~thread_pool() {
+        stop();
+    }
+}
+
+#endif
+
--- msr/thread/thread_pool.hpp
+++ msr/thread/thread_pool.hpp
@@ -11,7 +11,6 @@
 #include <thread>
 #include <boost/thread.hpp>
 #include <boost/utility.hpp>
-#include <msr/thread/detail/debug.hpp>
 #include <msr/thread/thread.hpp>
 #include <msr/thread/thread_pool_exception.hpp>
 
@@ -27,173 +26,38 @@ namespace msr {
         using readonly_lock = boost::shared_lock<mutex>;
         using readwrite_lock = boost::unique_lock<mutex>;
     public:
-        thread_pool(const std::size_t size = thread::hardware_concurrency()):
-            loop_(true), stopped_(true), size_(size) {
-            run();
-        }
+        thread_pool(const std::size_t size = thread::hardware_concurrency());
     public:
-        void run() {
-            readwrite_lock lock(thread_mutex_);
-            if (!stopped()) {
-                return;
-            }
-            loop_ = true;
-            for (unsigned int i = 0; i < size_; i++) {
-                threads_.insert(std::unique_ptr<thread>(new thread(&self::task_loop, this)));
-            }
-            stopped_ = false;
-        }
-        void stop() {
-            readwrite_lock thread_lock(thread_mutex_);
-            if (unsafe_this_thread_is_in()) {
-                throw exception(1, "stop(): Thread is trying to join itself.");
-            }
-            if (stopped()) {
-                return;
-            }
-            {
-                readwrite_lock task_lock(task_mutex_);
-                loop_ = false;
-                unsafe_clear_task_queue();
-                new_task_posted_.notify_all();
-            }
-            for (auto &t : threads_) {
-                try {
-                    t->join();
-                } catch (...) {
-                    t->detach();
-                }
-            }
-            threads_.clear();
-            stopped_ = true;
-        }
-        bool stopped() const {
-            return stopped_;
-        }
-        bool this_thread_is_in() const {
-            readonly_lock lock(thread_mutex_);
-            return unsafe_this_thread_is_in();
-        }
-        void post(task task_) {
-            readwrite_lock lock(task_mutex_);
-            task_queue_.push_back(task_);
-            new_task_posted_.notify_one();
-        }
-        void wait() const {
-            readonly_lock lock(task_mutex_);
-            if (unsafe_this_thread_is_in()) {
-                throw exception(2, "wait(): Thread is trying to wait itself.");
-            }
-            if (!unsafe_task_queue_is_empty()) {
-                task_queue_is_empty_.wait(lock);
-            }
-        }
-        void clear_task_queue() {
-            readwrite_lock lock(task_mutex_);
-            unsafe_clear_task_queue();
-            task_queue_is_empty_.notify_all();
-        }
-        bool task_queue_is_empty() const {
-            readonly_lock lock(task_mutex_);
-            return unsafe_task_queue_is_empty();
-        }
-        std::size_t size() const {
-            return size_;
-        }
-        void resize(const std::size_t size) {
-            if (!stopped()) {
-                throw exception(3, "resize(): Trying to resize a running thread pool.");
-            }
-            size_ = size;
-        }
+        void run();
+        void stop();
+        void post(task task_);
+        void resize(const std::size_t size);
+        void clear_task_queue();
+        void wait() const;
+        std::size_t size() const;
+        bool stopped() const;
+        bool task_queue_is_empty() const;
+        bool this_thread_is_in() const;
     private:
-        void task_loop() {
-            using namespace boost::chrono;
-            using namespace this_thread;
-            while (loop_) {
-                task _task;
-                {
-                    readwrite_lock lock(task_mutex_);
-                    /**
-                     * 'while' and '_loop' are required!
-                     * try to think about this scene.
-                     * thread_a is waiting for a new task.
-                     * then a new task comming, but thread_b
-                     * is holding the lock, and thread_b
-                     * finds that the task queue is not empty,
-                     * so thread_b tries to get the task.
-                     * meanwhile, thread_a is not blocking
-                     * anymore, so thread_a is will also try
-                     * to get the task. when one gets the task,
-                     * another's action will cause a iterator
-                     * reference error. so we use 'while' to
-                     * prevent this situation.
-                     */
-                    while (unsafe_task_queue_is_empty() && loop_) {
-#if MSR_THREAD_DEBUG(THREAD_POOL)
-                        std::stringstream ss;
-                        ss << "msr::thread::thread_pool: " << "[" << get_id() << ":wait]";
-                        printf("%s\n", ss.str().c_str());
-#endif
-                        new_task_posted_.wait(lock);
-                    }
-                    if (!loop_) {
-                        break;
-                    }
-                    _task = task_queue_.front();
-                    task_queue_.pop_front();
-                    if (unsafe_task_queue_is_empty()) {
-                        task_queue_is_empty_.notify_all();
-                    }
-                }
-#if MSR_THREAD_DEBUG(THREAD_POOL)
-                auto begin = high_resolution_clock::now();
-#endif
-                _task();
-#if MSR_THREAD_DEBUG(THREAD_POOL)
-                auto end = high_resolution_clock::now();
-                auto duration = end - begin;
-                std::stringstream ss;
-                ss << "msr::thread::thread_pool: " << "[" << get_id() << ":done] " << duration_cast<milliseconds>(duration).count() << "ms";
-                printf("%s\n", ss.str().c_str());
-#endif
-            }
-#if MSR_THREAD_DEBUG(THREAD_POOL)
-            std::stringstream ss;
-            ss << "msr::thread::thread_pool: " << "[" << get_id() << ":exit]";
-            printf("%s\n", ss.str().c_str());
-#endif
-        }
-        bool unsafe_this_thread_is_in() const {
-            auto id = this_thread::get_id();
-            for (const auto &t : threads_) {
-                if (t->get_id() == id) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        void unsafe_clear_task_queue() {
-            task_queue_.clear();
-        }
-        bool unsafe_task_queue_is_empty() const {
-            return task_queue_.empty();
-        }
+        void task_loop();
+        void unsafe_clear_task_queue();
+        bool unsafe_task_queue_is_empty() const;
+        bool unsafe_this_thread_is_in() const;
     public:
-        virtual ~thread_pool() {
-            stop();
-        }
+        virtual ~thread_pool();
     private:
-        std::atomic<bool> stopped_;
         std::atomic<bool> loop_;
-        std::set<std::unique_ptr<thread>> threads_;
-        std::deque<task> task_queue_;
+        std::atomic<bool> stopped_;
         std::atomic<std::size_t> size_;
-        mutable std::condition_variable_any new_task_posted_;
-        mutable std::condition_variable_any task_queue_is_empty_;
-        mutable mutex thread_mutex_;
+        std::deque<task> task_queue_;
+        std::set<std::unique_ptr<thread>> threads_;
         mutable mutex task_mutex_;
+        mutable mutex thread_mutex_;
+        mutable std::condition_variable_any task_queue_is_empty_;
+        mutable std::condition_variable_any new_task_posted_;
     };
 }
+
+#include <msr/thread/detail/thread_pool.ipp>
 
 #endif
