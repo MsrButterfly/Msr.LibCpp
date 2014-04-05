21d00a5016cdde978dea7b62471daefc8477ad85
Darren Liu <butterfly@msrlab.org>
2014-04-05 22:50:50 +0800

    [Network] Moved include sequence to boost-like style.

--- msr/network.hpp
+++ msr/network.hpp
@@ -1,26 +1,6 @@
 #ifndef MSR_NETWORK_HPP_INCLUDED
 #define MSR_NETWORK_HPP_INCLUDED
 
-#include <msr/network/client.hpp>
-#include <msr/network/client_exception.hpp>
-#include <msr/network/client_observer.hpp>
-#include <msr/network/connection.hpp>
-#include <msr/network/data.hpp>
-#include <msr/network/data_exception.hpp>
-#include <msr/network/detail/client_base.hpp>
-#include <msr/network/detail/connection_base.hpp>
-#include <msr/network/detail/observer_base.hpp>
-#include <msr/network/detail/server_base.hpp>
-#include <msr/network/error.hpp>
-#include <msr/network/observer.hpp>
-#include <msr/network/protocol.hpp>
-#include <msr/network/server.hpp>
-#include <msr/network/server_exception.hpp>
-#include <msr/network/server_observer.hpp>
-#include <msr/network/tcp_ip_client.hpp>
-#include <msr/network/tcp_ip_client_observer.hpp>
-#include <msr/network/tcp_ip_connection.hpp>
-#include <msr/network/tcp_ip_server.hpp>
-#include <msr/network/tcp_ip_server_observer.hpp>
+#include <msr/network/network.hpp>
 
 #endif
