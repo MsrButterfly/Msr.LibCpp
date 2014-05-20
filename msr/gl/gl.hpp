2e20f0ff97508f6549d8b35501b6ff1c768b384d
Darren Liu <butterfly@msrlab.org>
2014-05-20 16:56:44 +0800

    [GL] Headers were sorted.

--- msr/gl/gl.hpp
+++ msr/gl/gl.hpp
@@ -1,9 +1,11 @@
 #ifndef MSR_GL_GL_HPP_INCLUDED
 #define MSR_GL_GL_HPP_INCLUDED
 
-#include <msr/gl/shader.hpp>
-#include <msr/gl/program.hpp>
 #include <msr/gl/init.hpp>
 #include <msr/gl/output.hpp>
+#include <msr/gl/program.hpp>
+#include <msr/gl/shader.hpp>
+#include <msr/gl/uniform.hpp>
+#include <msr/gl/utility.hpp>
 
 #endif
