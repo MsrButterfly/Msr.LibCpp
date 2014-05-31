971f9324af1c0c6cbd88aa5733025abe66c47f1c
Darren Liu <butterfly@msrlab.org>
2014-06-01 01:25:14 +0800

    [Number] Headers were sorted.

--- msr/number/number.hpp
+++ msr/number/number.hpp
@@ -2,10 +2,12 @@
 #define MSR_NUMBER_NUMBER_HPP_INCLUDED
 
 #include "absolute.hpp"
-#include "exception.hpp"
-#include "large_int.hpp"
 #include "bit.hpp"
 #include "digit.hpp"
+#include "divide_by_zero.hpp"
 #include "division.hpp"
+#include "exception.hpp"
+#include "large_int_divide_by_zero.hpp"
+#include "large_int.hpp"
 
 #endif
