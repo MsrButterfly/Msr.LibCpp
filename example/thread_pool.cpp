db20111b6f382ec27ecb9fb4c54619d9597831a2
Darren Liu <butterfly@msrlab.org>
2015-10-18 19:35:44 +0800

    [Thread] Updated examples.

--- example/thread_pool.cpp
+++ example/thread_pool.cpp
@@ -1,25 +1,32 @@
+#include <chrono>
+#include <iostream>
+#include <sstream>
+#include <thread>
 #include <msr/thread.hpp>
 
 int main(int argc, const char *argv[]) {
+    using namespace std;
+    using namespace this_thread;
+    using namespace chrono;
     using namespace msr;
-    using namespace std::this_thread;
-    using namespace std::chrono;
     try {
-        for (std::size_t i = 1; i <= 2 * msr::thread::hardware_concurrency(); ++i) {
+        for (size_t i = 1; i <= 2 * thread::hardware_concurrency(); ++i) {
             thread_pool pool(i);
-            auto begin = std::chrono::high_resolution_clock::now();
-            for (std::size_t j = 0; j < 1000; ++j) {
+            auto begin = high_resolution_clock::now();
+            for (size_t j = 0; j < 1000; ++j) {
                 pool.post([]() {
-                    for (std::size_t i = 0; i < 1000000; ++i);
+                    for (size_t i = 0; i < 1000000; ++i);
                 });
             }
             pool.wait();
-            auto end = std::chrono::high_resolution_clock::now();
+            auto end = high_resolution_clock::now();
             auto duration = end - begin;
-            printf("%zd threads: Finished in %lldms.\n", i, std::chrono::duration_cast<milliseconds>(duration).count());
+            printf("%zd threads: Finished in %lldms.\n", i, duration_cast<milliseconds>(duration).count());
         }
-    } catch (thread_pool::exception &e) {
-        fprintf(stderr, "%s\n", e.description());
+    } catch (reverse_control &e) {
+        stringstream ss;
+        ss << e;
+        fprintf(stderr, "%s\n", ss.str().c_str());
     } catch (std::exception &e) {
         fprintf(stderr, "%s\n", e.what());
     }
